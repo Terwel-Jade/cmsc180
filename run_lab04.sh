6dafcaf14b5c3a4298ba6746926dc217465b26ac
Jade Brian Terwel <jaterwel@up.edu.ph>
2026-04-13 14:09:27 +0800

    feat: added configured pcs

--- run_lab04.sh
+++ run_lab04.sh
@@ -2,6 +2,28 @@
 
 DIRECTORY="Desktop/temp"
 BINARY="lab04"
+SIZE=16000
+RUNS=1
+
+for ((i=1; i<=RUNS; i++))
+do 
+    echo "Iteration $i --------------"
+
+    ssh 10.0.4.238 "cd $DIRECTORY && ./$BINARY $SIZE 5002 1" &
+
+    ssh 10.0.4.69 "cd $DIRECTORY && ./$BINARY $SIZE 5001 1" &
+    
+    ssh 10.0.4.140 "cd $DIRECTORY && ./$BINARY $SIZE 5003 1" &
+
+    ssh 10.0.4.50 "cd $DIRECTORY && ./$BINARY $SIZE 5004 1" &
+
+    ./$BINARY $SIZE 5000 0 | sed $'s/.*/\\e[32m&\\e[0m/'
+
+    wait
+done#!/bin/bash
+
+DIRECTORY="Desktop/temp"
+BINARY="lab04"
 SIZE=4000
 RUNS=1
 
